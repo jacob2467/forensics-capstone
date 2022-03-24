26185ba56fc9c8f2c6e6bfd9656e43fa22676ce5
jacob2467 <92223080+jacob2467@users.noreply.github.com>
2022-03-23 22:56:09 -0400

    Update project.py

--- project.py
+++ project.py
@@ -10,33 +10,42 @@ store information in binary, so the “1” is actually 00000001, the “2” is
 most computers either use a hard disk drive (HDD) or solid state drive (SSD). Due to the binary nature of computers, all forms of digital storage rely on binary forms
 of data storage. To understand this concept with an analogy, imagine taking a standardized test with four multiple choice questions that each have four possible
 answers (A, B, C, or D). If you answer B to question 1, you could represent that as 0100. In other words, A is not filled in, B is filled in, C is not, and D is not.
-Using this system, if a student answered A, B, C, D in that order, you could represent that as 1000 0100 0010 0001 (without the spaces)."""
+Using this system, if a student answered A, B, C, D in that order, you could represent that as 1000 0100 0010 0001 (without the spaces).\n"""
 
-main = ["How is digital data stored?", "Types of data storage", "How is data “lost” and how can it be recovered?"]
-typesOfStorage = ["Hard Disk Drives (HDDs)", "Solid State Drives (SSDs)", "How do SSDs compare to HDDs?"]
+main = ["What would you like to learn about? Type 'menu' at any time to return here." , "How is digital data stored?", "Types of data storage", "How is data “lost” and how can it be recovered?"]
+typesOfStorage = ["There are a variety of methods used to store digital information. Which would you like to learn about first?", "Hard Disk Drives (HDDs)", "Solid State Drives (SSDs)", "How do SSDs compare to HDDs?"]
 dataRecovery = []
 
-choice = input(f"""What would you like to learn about?
-    1. {main[0]}
-    2. {main[1]}
-    3. {main[2]}\n""")
+def prompt(options):
+    global choice
+    choice = input(f"""{options[0]}
+    1. {options[1]}
+    2. {options[2]}
+    3. {options[3]}\n""")
 
-if choice in answer_1:
-    print(HowIsDigitalDataStored)
+    if choice == "menu":
 
-elif choice in answer_2:
-    choice2 = input(f"""There are a variety of methods used to store digital information. Which would you like to learn about first?
-    1. {typesOfStorage[0]}
-    2. {typesOfStorage[1]}
-    3. {typesOfStorage[2]}\n""")
 
-    if choice2 in answer_1:
+    if choice in answer_1:
+        print(HowIsDigitalDataStored)
+        prompt(main)
+
+    elif choice in answer_2:
+        choice2 = input(f"""{typesOfStorage[0]}
+        1. {typesOfStorage[1]}
+        2. {typesOfStorage[2]}
+        3. {typesOfStorage[3]}\n""")
+
+        if choice2 in answer_1:
+            pass
+        if choice2 in answer_2:
+            pass
+        if choice2 in answer_3:
+            pass
+
+    elif choice in answer_3:
         pass
-    if choice2 in answer_2:
-        pass
-    if choice2 in answer_3:
-        pass
+prompt(main)
 
 
-elif choice in answer_3:
-    pass
+
